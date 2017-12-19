7aa24caa4983bc9cc1e347546027557adf757348
maximeLong <andyrogerkats@gmail.com>
2017-12-19 13:21:10 -0800

    grouped models can scale proportionally in menu now

--- Assets/HotmaxEditorVR/Resources/loadPropsMenu.cs
+++ Assets/HotmaxEditorVR/Resources/loadPropsMenu.cs
@@ -141,25 +141,66 @@ public class loadPropsMenu : MonoBehaviour
     //show the prop model on the tile and rotate it around
     void displayPropInMenu(GameObject propTile, GameObject menuProp, int page, int i)
     {
+        float biggestPropSide;
+
         GameObject menuPropDisplay = Instantiate(menuProp);
         menuPropDisplay.name = menuProp + " (visual)";
-        menuPropDisplay.transform.parent = propTile.transform;
-        menuPropDisplay.transform.rotation = propTile.transform.rotation;
-        menuPropDisplay.transform.localPosition = new Vector3(0, 0, 0);
-        menuPropDisplay.transform.localScale = menuPropDisplay.transform.localScale / 20;
 
-        //TODO: right now this scales the model down reletive to the bounding box of the propTile. it doesn't work for oblong objects
-        menuPropDisplay.transform.localScale = (propTile.GetComponent<Renderer>().bounds.size.magnitude) * menuPropDisplay.transform.localScale;
+        //create a bounding box so the prop can be scaled proportionally
+        GameObject boundingScaleBox = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        boundingScaleBox.GetComponent<MeshRenderer>().enabled = false;
+        boundingScaleBox.GetComponent<BoxCollider>().enabled = false;
+        boundingScaleBox.transform.position = propTile.transform.position;
 
-        //TODO: there is probably a better way to do this
-        if (menuPropDisplay.GetComponent<Rigidbody>())
+        Vector3 menuPropDimension = Vector3.zero;
+
+        //if the model has children, calculate the total bounds of all the children
+        if (menuProp.transform.childCount > 0)
         {
-            menuPropDisplay.GetComponent<Rigidbody>().isKinematic = true;
+            Bounds bounds = new Bounds(menuPropDisplay.transform.position, Vector3.zero);
+            foreach (Renderer renderer in menuProp.GetComponentsInChildren<Renderer>())
+            {
+                bounds.Encapsulate(renderer.bounds);
+            }
+            menuPropDimension = bounds.size;
         }
+        //only calculate the bounds of the prop
+        else
+        {
+            menuPropDimension = menuPropDisplay.GetComponent<Renderer>().bounds.size;
+        }
+
+        //compare the sides of the menu prop to find the largest side
+        //set the bounding scale box to fit around the menu prop
+        if (menuPropDimension.x >= menuPropDimension.y && menuPropDimension.x >= menuPropDimension.z)
+        {
+            biggestPropSide = menuPropDimension.x;
+            boundingScaleBox.transform.localScale = new Vector3(biggestPropSide, biggestPropSide, biggestPropSide);
+        }
+
+        if (menuPropDimension.y >= menuPropDimension.x && menuPropDimension.y >= menuPropDimension.z)
+        {
+            biggestPropSide = menuPropDimension.y;
+            boundingScaleBox.transform.localScale = new Vector3(biggestPropSide, biggestPropSide, biggestPropSide);
+        }
+
+        if (menuPropDimension.z >= menuPropDimension.x && menuPropDimension.z >= menuPropDimension.y)
+        {
+            biggestPropSide = menuPropDimension.z;
+            boundingScaleBox.transform.localScale = new Vector3(biggestPropSide, biggestPropSide, biggestPropSide);
+        }
+
+        menuPropDisplay.transform.parent = boundingScaleBox.transform;
+        menuPropDisplay.transform.localPosition = new Vector3(0, 0, 0);
+
+        boundingScaleBox.transform.parent = propTile.transform;
+        boundingScaleBox.transform.localScale = new Vector3(.6f, .6f, .6f);
+
         if (menuPropDisplay.GetComponent<Collider>())
         {
-            Destroy(menuPropDisplay.GetComponent<Collider>());
+            menuPropDisplay.GetComponent<Collider>().enabled = false;
         }
+
         menuPropDisplay.AddComponent<slowRotation>();
         menuPropDisplay.SetActive(true);
     }
