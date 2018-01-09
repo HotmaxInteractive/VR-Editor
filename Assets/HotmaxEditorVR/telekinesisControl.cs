dd6f785cd63ea98c730fd91138e58bd6fba0200c
maximeLong <andyrogerkats@gmail.com>
2018-01-08 18:00:19 -0800

    added lerping to telekinesisControl

--- Assets/HotmaxEditorVR/telekinesisControl.cs
+++ Assets/HotmaxEditorVR/telekinesisControl.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class telekinesisControl : MonoBehaviour
 {
-
     private float initialPadYPosition;
     private float currentPadYPos;
 
     private float distToController;
-    [SerializeField]
-    private float scrollSpeed = .2f;
     private float scrollDistance = 0;
 
+    private float scrollSpeed = .2f;
+    private float tweenSpeed = 4;
+    private float tweenDistance;
+
+    private GameObject tweenAnchor;
 
     private void OnEnable()
     {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.GetComponent<MeshRenderer>().enabled = false;
+        cube.GetComponent<BoxCollider>().enabled = false;
+        tweenAnchor = cube;
+
         //this gets the initial offset of the object to the controller
         //the purpose being to start the object off at the same place it was in before selecting it
         initialPadYPosition = inputManager.selectorHand.GetAxis().y;
@@ -25,19 +32,24 @@ public class telekinesisControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
+        tweenDistance = Vector3.Distance(transform.position, tweenAnchor.transform.position) * Time.deltaTime;
         currentPadYPos = inputManager.selectorHand.GetAxis().y;
 
         Vector3 offset = inputManager.hand2.transform.forward * (distToController + scrollDistance);
-        transform.position = inputManager.hand2.transform.position + offset;
+        tweenAnchor.transform.position = inputManager.hand2.transform.position + offset;
+        //--easing created by "tweenDistance" -a larger tweenDistance will make a faster tween
+        transform.position = Vector3.MoveTowards(transform.position, tweenAnchor.transform.position, tweenDistance * tweenSpeed);
 
-        if (currentPadYPos > initialPadYPosition)
+        print(Vector3.Distance(transform.position, tweenAnchor.transform.position));
+
+        if (currentPadYPos > initialPadYPosition + .05f)
         {
             scrollDistance += scrollSpeed;
             initialPadYPosition = currentPadYPos;
         }
-        if (currentPadYPos < initialPadYPosition)
+        if (currentPadYPos < initialPadYPosition - .05f)
         {
             scrollDistance -= scrollSpeed;
             initialPadYPosition = currentPadYPos;
