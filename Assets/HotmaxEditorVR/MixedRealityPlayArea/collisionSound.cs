422a807ffab309446112957dc53de364d9627859
Andy-Roger <andyrogerkats@gmail.com>
2018-02-01 15:29:03 -0800

    added changes to mixedRealityPlayArea

---
+++ Assets/HotmaxEditorVR/MixedRealityPlayArea/collisionSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionSound : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(GetComponent<AudioSource>())
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
