c1d33504a9b7e47b0c000019f2ce35a77e701cde
Andy-Roger <andyrogerkats@gmail.com>
2018-02-05 07:52:55 -0800

    workable demo

---
+++ Assets/HotmaxEditorVR/MixedRealityPlayArea/syncingAssetsDemo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class syncingAssetsDemo : MonoBehaviour
+{
+    public GameObject syncingAssetsText;
+    public GameObject propInMenu;
+
+    void Start()
+    {
+        inputManager.trackedController2.MenuButtonClicked += menuButtonClicked;
+    }
+
+    private void OnApplicationQuit()
+    {
+        inputManager.trackedController2.MenuButtonClicked -= menuButtonClicked;
+    }
+
+    void menuButtonClicked(object sender, ClickedEventArgs e)
+    {
+        syncingAssetsText.SetActive(true);
+        Invoke("showAssetInMenu", 2);
+    }
+
+    void showAssetInMenu()
+    {
+        syncingAssetsText.SetActive(false);
+        propInMenu.SetActive(true);
+    }
+}
