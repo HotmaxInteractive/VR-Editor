ee5fef857d2ebdacb3e2bc59d0da97b0ef62584e
Andy-Roger <andyrogerkats@gmail.com>
2018-02-15 16:39:38 -0800

    fixd telekinesis lerp bug

--- Assets/HotmaxEditorVR/telekinesisControl.cs
+++ Assets/HotmaxEditorVR/telekinesisControl.cs
@@ -16,37 +16,26 @@ public class telekinesisControl : MonoBehaviour
     private float tweenSpeed = 4;
     private float tweenDistance;
 
-    private GameObject tweenToPosition;
+    private Vector3 tweenToPosition;
 
     private void OnEnable()
     {
-        tweenToPosition = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        tweenToPosition.GetComponent<MeshRenderer>().enabled = false;
-        tweenToPosition.GetComponent<BoxCollider>().enabled = false;
-
-        //this gets the initial offset of the object to the controller
-        //the purpose being to start the object off at the same place it was in before selecting it
+        //Gets the initial offset of the object to the controller
         initialPadYPosition = inputManager.selectorHand.GetAxis().y;
         distToController = Vector3.Distance(transform.position, inputManager.hand2.transform.position);
         scrollDistance = 0;
         init.rotationGizmos.SetActive(false);
     }
 
-    private void OnDisable()
-    {
-        Destroy(tweenToPosition);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        tweenDistance = Vector3.Distance(transform.position, tweenToPosition.transform.position) * Time.deltaTime;
+        tweenDistance = Vector3.Distance(transform.position, tweenToPosition) * Time.deltaTime;
         currentPadYPos = inputManager.selectorHand.GetAxis().y;
 
         Vector3 offset = inputManager.hand2.transform.forward * (distToController + scrollDistance);
-        tweenToPosition.transform.position = inputManager.hand2.transform.position + offset;
+        tweenToPosition = inputManager.hand2.transform.position + offset;
         //--easing created by "tweenDistance" -a larger tweenDistance will make a faster tween
-        transform.position = Vector3.MoveTowards(transform.position, tweenToPosition.transform.position, tweenDistance * tweenSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, tweenToPosition, tweenDistance * tweenSpeed);
 
         if (currentPadYPos > initialPadYPosition + .1f)
         {
@@ -54,18 +43,10 @@ public class telekinesisControl : MonoBehaviour
             initialPadYPosition = currentPadYPos;
         }
 
-        if (tweenToPosition.transform.position.magnitude < inputManager.hand2.transform.position.magnitude + 0.3f)
+        if (currentPadYPos < initialPadYPosition - .1f)
         {
-            tweenToPosition.transform.position = inputManager.hand2.transform.forward / 2;
-            return;
-        }
-        else
-        {
-            if (currentPadYPos < initialPadYPosition - .1f)
-            {
-                scrollDistance -= scrollSpeed;
-                initialPadYPosition = currentPadYPos;
-            }
+            scrollDistance -= scrollSpeed;
+            initialPadYPosition = currentPadYPos;
         }
     }
 }
