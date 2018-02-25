a73aeae9772841012888a2408695f7886f88a3ee
Andy-Roger <andyrogerkats@gmail.com>
2018-02-25 11:56:53 -0800

    cleaned up rotation code

--- Assets/HotmaxEditorVR/builder/prop/rotationControl.cs
+++ Assets/HotmaxEditorVR/builder/prop/rotationControl.cs
@@ -10,12 +10,12 @@ public class rotationControl : MonoBehaviour
 
     //--vars used during rotation
     private bool initialHit = false;
-    private Transform initialParent;
     private GameObject lookAtRaycast;
     private GameObject stationaryLookAtRaycast;
     private GameObject propRotationHolder;
     private Vector3 lookPos;
-    private Quaternion rotation;
+    private string rotationGizmo = "RotationGizmo";
+    private string rotationAxisCollider = "pie";
 
     //--Handles turning off and on Gizmo axis
     private List<Transform> rotationAxis = new List<Transform>();
@@ -26,6 +26,92 @@ public class rotationControl : MonoBehaviour
         Z,
         ALL,
         NONE
+    }
+
+    private void Start()
+    {
+        //holder for prop during rotation
+        propRotationHolder = Instantiate(Resources.Load("propRotationHolder", typeof(GameObject))) as GameObject;
+        //--rotation offset gages
+        lookAtRaycast = Instantiate(Resources.Load("rotationGage", typeof(GameObject))) as GameObject;
+        stationaryLookAtRaycast = Instantiate(Resources.Load("rotationGage", typeof(GameObject))) as GameObject;
+    }
+
+    private void OnEnable()
+    {
+        stateManager.rotationGizmoIsSelectedEvent += updateRotationGizmoIsSelected;
+        stateManager.rotationModeEvent += updateRotationModeEvent;
+        inputManager.trackedController2.TriggerUnclicked += triggerUnclicked;
+
+        init.rotationGizmos.SetActive(true);
+        init.rotationGizmos.transform.position = transform.position;
+    }
+
+    protected virtual void OnDisable()
+    {
+        stateManager.rotationGizmoIsSelectedEvent -= updateRotationGizmoIsSelected;
+        stateManager.rotationModeEvent -= updateRotationModeEvent;
+        inputManager.trackedController2.TriggerUnclicked -= triggerUnclicked;
+
+        if (init.rotationGizmos != null)
+        {
+            init.rotationGizmos.SetActive(false);
+        }
+    }
+
+    void updateRotationGizmoIsSelected(bool value)
+    {
+        _rotationGizmoIsSelected = value;
+        setRotationGizmoVisible(visibleRotation.ALL);
+
+        if (_rotationGizmoIsSelected)
+        {
+            lookAtRaycast.SetActive(true);
+            stationaryLookAtRaycast.SetActive(true);
+            lookAtRaycast.transform.position = transform.position;
+            stationaryLookAtRaycast.transform.position = transform.position;
+            propRotationHolder.transform.position = transform.position;
+
+            initialHit = true;
+        }
+        else
+        {
+            //re-parent back in props
+            transform.parent = init.props.transform;
+            lookAtRaycast.SetActive(false);
+            stationaryLookAtRaycast.SetActive(false);
+        }
+    }
+
+    void updateRotationModeEvent(stateManager.rotationModes value)
+    {
+        _rotationMode = value;
+        if (_rotationGizmoIsSelected)
+        {
+            switch (_rotationMode)
+            {
+                case stateManager.rotationModes.xRotationMode:
+                    setRotationGizmoVisible(visibleRotation.X);
+                    init.rotationGizmos.transform.Find("x" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(true);
+                    break;
+                case stateManager.rotationModes.yRotationMode:
+                    setRotationGizmoVisible(visibleRotation.Y);
+                    init.rotationGizmos.transform.Find("y" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(true);
+                    break;
+                case stateManager.rotationModes.zRotationMode:
+                    setRotationGizmoVisible(visibleRotation.Z);
+                    init.rotationGizmos.transform.Find("z" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(true);
+                    break;
+            }
+        }
+    }
+
+    void triggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        init._stateManagerMutatorRef.SET_ROTATION_GIZMO_IS_SELECTED(false);
+        init.rotationGizmos.transform.Find("x" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(false);
+        init.rotationGizmos.transform.Find("y" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(false);
+        init.rotationGizmos.transform.Find("z" + rotationGizmo).transform.Find(rotationAxisCollider).gameObject.SetActive(false);
     }
 
     void Update()
@@ -60,11 +146,9 @@ public class rotationControl : MonoBehaviour
 
     void doRotation(RaycastHit hit, Vector3 faceDirection, float lockAxis)
     {
-        //on raycast + click trigger preserve this objects original rotation by parenting other object
         if (initialHit)
         {
-
-            //do lookAt so the the perpendicular face to the locked axis faces the hit.point
+            //--initialize holder and gage rotations / positions
             lookAtRaycast.transform.LookAt(hit.point, faceDirection);
             stationaryLookAtRaycast.transform.LookAt(hit.point, faceDirection);
             propRotationHolder.transform.LookAt(hit.point, faceDirection);
@@ -72,115 +156,22 @@ public class rotationControl : MonoBehaviour
             transform.parent = propRotationHolder.transform;
             initialHit = false;
         }
+
         lookPos = hit.point - lookAtRaycast.transform.position;
-        //lock the rotation to just one axis
+        //locks the rotation to just one axis
         lockAxis = 0;
-        //lookAtRaycast follows the hit.point with locked axis
-        rotation = Quaternion.LookRotation(lookPos, faceDirection);
 
-        lookAtRaycast.transform.rotation = Quaternion.Slerp(lookAtRaycast.transform.rotation, rotation, Time.deltaTime * 3);
-        propRotationHolder.transform.rotation = Quaternion.Slerp(lookAtRaycast.transform.rotation, rotation, Time.deltaTime * 3);
+        Quaternion rotateDirection = Quaternion.LookRotation(lookPos, faceDirection);
 
-    }
-
-    private void Start()
-    {
-        propRotationHolder = Instantiate(Resources.Load("propRotationHolder", typeof(GameObject))) as GameObject;
-        //set up the container for our object to rotate
-        lookAtRaycast = Instantiate(Resources.Load("lookAtRaycast", typeof(GameObject))) as GameObject;
-        //This should have a different apearance than the "lookAtRaycast object"
-        stationaryLookAtRaycast = Instantiate(Resources.Load("lookAtRaycast", typeof(GameObject))) as GameObject;
-
-    }
-
-    private void OnEnable()
-    {
-       
-        stateManager.rotationGizmoIsSelectedEvent += updateRotationGizmoIsSelected;
-        stateManager.rotationModeEvent += updateRotationModeEvent;
-        inputManager.trackedController2.TriggerUnclicked += triggerUnclicked;
-
-        //handle the visual representation of the rotation gizmo
-        init.rotationGizmos.SetActive(true);
-        init.rotationGizmos.transform.position = transform.position;
-    }
-
-    protected virtual void OnDisable()
-    {
-        stateManager.rotationGizmoIsSelectedEvent -= updateRotationGizmoIsSelected;
-        stateManager.rotationModeEvent -= updateRotationModeEvent;
-        inputManager.trackedController2.TriggerUnclicked -= triggerUnclicked;
-
-        //throws error when the app quits if we don't check if it is not null
-        if (init.rotationGizmos != null)
-        {
-            init.rotationGizmos.SetActive(false);
-        }
-    }
-
-    void updateRotationGizmoIsSelected(bool value)
-    {
-        _rotationGizmoIsSelected = value;
-        setRotationGizmoVisible(visibleRotation.ALL);
-
-        if (_rotationGizmoIsSelected)
-        {
-            lookAtRaycast.SetActive(true);
-            stationaryLookAtRaycast.SetActive(true);
-            lookAtRaycast.transform.position = transform.position;
-            stationaryLookAtRaycast.transform.position = transform.position;
-
-            propRotationHolder.transform.position = transform.position;
-
-
-            //save current parent
-            initialHit = true;
-        }
-        else
-        {
-            //unparent object before destroying
-            transform.parent = init.props.transform;
-            lookAtRaycast.SetActive(false);
-            stationaryLookAtRaycast.SetActive(false);
-        }
-    }
-
-    void updateRotationModeEvent(stateManager.rotationModes value)
-    {
-        _rotationMode = value;
-        if (_rotationGizmoIsSelected)
-        {
-            switch (_rotationMode)
-            {
-                case stateManager.rotationModes.xRotationMode:
-                    setRotationGizmoVisible(visibleRotation.X);
-                    init.rotationGizmos.transform.Find("xRotationGizmo").transform.Find("pie").gameObject.SetActive(true);
-                    break;
-                case stateManager.rotationModes.yRotationMode:
-                    setRotationGizmoVisible(visibleRotation.Y);
-                    init.rotationGizmos.transform.Find("yRotationGizmo").transform.Find("pie").gameObject.SetActive(true);
-                    break;
-                case stateManager.rotationModes.zRotationMode:
-                    setRotationGizmoVisible(visibleRotation.Z);
-                    init.rotationGizmos.transform.Find("zRotationGizmo").transform.Find("pie").gameObject.SetActive(true);
-                    break;
-            }
-        }
-    }
-
-    void triggerUnclicked(object sender, ClickedEventArgs e)
-    {
-        init._stateManagerMutatorRef.SET_ROTATION_GIZMO_IS_SELECTED(false);
-        init.rotationGizmos.transform.Find("xRotationGizmo").transform.Find("pie").gameObject.SetActive(false);
-        init.rotationGizmos.transform.Find("yRotationGizmo").transform.Find("pie").gameObject.SetActive(false);
-        init.rotationGizmos.transform.Find("zRotationGizmo").transform.Find("pie").gameObject.SetActive(false);
+        lookAtRaycast.transform.rotation = Quaternion.Slerp(lookAtRaycast.transform.rotation, rotateDirection, Time.deltaTime * 3);
+        propRotationHolder.transform.rotation = Quaternion.Slerp(propRotationHolder.transform.rotation, rotateDirection, Time.deltaTime * 3);
     }
 
     void setRotationGizmoVisible(visibleRotation visibleRotation)
     {
-        Transform xGizmo = init.rotationGizmos.transform.Find("xRotationGizmo");
-        Transform yGizmo = init.rotationGizmos.transform.Find("yRotationGizmo");
-        Transform zGizmo = init.rotationGizmos.transform.Find("zRotationGizmo");
+        Transform xGizmo = init.rotationGizmos.transform.Find("x" + rotationGizmo);
+        Transform yGizmo = init.rotationGizmos.transform.Find("y" + rotationGizmo);
+        Transform zGizmo = init.rotationGizmos.transform.Find("z" + rotationGizmo);
 
         rotationAxis.Add(xGizmo);
         rotationAxis.Add(yGizmo);
@@ -196,11 +187,9 @@ public class rotationControl : MonoBehaviour
             case visibleRotation.X:
                 rotationAxis[0].gameObject.SetActive(true);
                 break;
-
             case visibleRotation.Y:
                 rotationAxis[1].gameObject.SetActive(true);
                 break;
-
             case visibleRotation.Z:
                 rotationAxis[2].gameObject.SetActive(true);
                 break;
