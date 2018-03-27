6adc4ddcfaeb3561c17fd0a6016b6e8e8d3833c1
Andy-Roger <andyrogerkats@gmail.com>
2018-03-27 15:05:03 -0700

    added other calibration cube

--- Assets/HotmaxEditorVR/presentation/offsetCalibration/fastCalibrationSetup.cs
+++ Assets/HotmaxEditorVR/presentation/offsetCalibration/fastCalibrationSetup.cs
@@ -13,8 +13,13 @@ public class fastCalibrationSetup : MonoBehaviour
 
     public ZEDPointCloudManager pointCloudManager;
 
-    public Transform calibrationCube;
+    public Transform posCalibrationCube;
+    public Transform rotCalibrationCube;
+
     public Transform virtualCameraPos;
+
+    private bool nearRotCube = false;
+    private bool nearPosCube = false;
 
     private void Start()
     {
@@ -34,39 +39,51 @@ public class fastCalibrationSetup : MonoBehaviour
         //save position and rotation on initial click
         //...move controller...
         //on unclick apply the difference in before and after to Zed Greenscreen
-        if (Vector3.Distance(_trackedController2.transform.position, calibrationCube.position) < 0.25f && calibrationCube.gameObject.activeInHierarchy)
+        if (Vector3.Distance(_trackedController2.transform.position, posCalibrationCube.position) < 0.1f && posCalibrationCube.gameObject.activeInHierarchy)
         {
+            nearPosCube = true;
             initialControllerPosition = _trackedController2.transform.position;
-            initialControllerRotation = _trackedController2.transform.rotation;
+            pointCloudManager.update = false;
+        }
 
+        if (Vector3.Distance(_trackedController2.transform.position, rotCalibrationCube.position) < 0.1f && rotCalibrationCube.gameObject.activeInHierarchy)
+        {
+            nearRotCube = true;
+            initialControllerRotation = _trackedController2.transform.rotation;
             pointCloudManager.update = false;
         }
     }
 
     void triggerUnclicked(object sender, ClickedEventArgs e)
     {
-        if(!pointCloudManager.update)
+        if (!pointCloudManager.update)
         {
+            if(nearPosCube)
+            {
+                //--find the difference between CONTROLLER initial pos and current pos
+                Vector3 positionOffset = initialControllerPosition - _trackedController2.transform.position;
+                Zed_Greenscreen.transform.position += positionOffset * 0.2f;
+                virtualCameraPos.position = Zed_Greenscreen.transform.position;
+            }
+            else if(nearRotCube)
+            {
+                //--find difference in rotation
+                Quaternion rotationOffset = initialControllerRotation * Quaternion.Inverse(_trackedController2.transform.rotation); //deltaC = A * Quaternion.Inverse(B);
+                Quaternion appliedDifference = rotationOffset * Zed_Greenscreen.transform.rotation; //add difference to D = C * D;
 
-            //--find the difference between CONTROLLER initial pos and current pos
-            Vector3 positionOffset = initialControllerPosition - _trackedController2.transform.position;
-            Zed_Greenscreen.transform.position += positionOffset * 0.2f;
+                // The step size is equal to speed times frame time.
+                float step = 1 * Time.deltaTime;
 
-            //--find difference in rotation
-            Quaternion rotationOffset = initialControllerRotation * Quaternion.Inverse(_trackedController2.transform.rotation); //deltaC = A * Quaternion.Inverse(B);
-            Quaternion appliedDifference = rotationOffset * Zed_Greenscreen.transform.rotation; //add difference to D = C * D;
+                Vector3 newDir = Vector3.RotateTowards(Zed_Greenscreen.transform.forward, virtualCameraPos.forward, step, 0.0f);
 
-            // The step size is equal to speed times frame time.
-            float step = 1 * Time.deltaTime;
-
-            virtualCameraPos.eulerAngles = appliedDifference.eulerAngles;
-            virtualCameraPos.position = Zed_Greenscreen.transform.position;
-
-            Vector3 newDir = Vector3.RotateTowards(Zed_Greenscreen.transform.forward, virtualCameraPos.forward, step, 0.0f);
-
-            // Move our position a step closer to the target.
-            Zed_Greenscreen.transform.rotation = Quaternion.LookRotation(newDir);
-            pointCloudManager.update = true;
+                // Move our position a step closer to the target.
+                Zed_Greenscreen.transform.rotation = Quaternion.LookRotation(newDir);
+                virtualCameraPos.eulerAngles = appliedDifference.eulerAngles;
+            }
         }
+
+        pointCloudManager.update = true;
+        nearPosCube = false;
+        nearRotCube = false;
     }
 }
